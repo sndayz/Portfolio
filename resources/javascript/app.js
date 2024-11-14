2bb3ae585a46beb6c54d638d45650576c3c3966f
Yuri <yuriolivieri2000@gmail.com>
2024-11-14 17:27:16 +0100

    mobile version code

---
+++ resources/javascript/app.js
@@ -0,0 +1,16 @@
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if(entry.isIntersecting){
+            entry.target.classList.add('show');
+        }
+    });
+});
+
+const hiddenElements = document.querySelectorAll(".hidden");
+hiddenElements.forEach((el) => observer.observe(el));
+
+const turnBlue = () => button.style.backgroundColor = 'blue';
+
+const button = document.querySelector(".submit");
+button.onclick = turnBlue;
